319e0f3ab2112102ebfddba61817f8e8ccc48127
WingedWhite <bartkowiak.marc@gmail.com>
2022-08-25 13:07:48 +0200

    Making of a Alert

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("button");
+var buttonText = document.getElementById("textInButton");
+var buttonColor;
+var buttonTextColor;
+
+function proba()
+{
+    buttonText.style.background = yellow;
+}
