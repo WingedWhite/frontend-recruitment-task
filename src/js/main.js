7c6922416396304060a17f84b41c195199d91e05
WingedWhite <bartkowiak.marc@gmail.com>
2022-08-25 18:47:15 +0200

    Liczy bez pamiętania

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,42 @@
-var ClicksNumber = 0;
-document.cookie = "clicks = 0;";
+let ClicksNumber;
 
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
 
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+if(getCookie('numer') === Number)
+{
+    ClicksNumber = getCookie('numer');
+}
+else
+{
+    ClicksNumber = 0;
+}
 
 function show()
 {
     Overlay.style.visibility = 'visible';
     Alert.style.visibility = 'visible';
     ClicksNumber++;
-    CNumber.innerText[0] = ClicksNumber;
+    CNumber.innerText = ClicksNumber + " Clicks";
 }
 
 function hide()
