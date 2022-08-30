38655eaf2ce0d053cbc38149e001de8285b12d0a
WingedWhite <bartkowiak.marc@gmail.com>
2022-08-30 17:11:46 +0200

    kolejne porządki

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,10 @@
 var ClicksNumber;
 
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
 if(localStorage.getItem("numer") !== undefined)
 {
   ClicksNumber = localStorage.getItem("numer");
   localStorage.removeItem("numer");
+  
 }
 else
 {
@@ -51,8 +29,6 @@ function hide()
   Overlay.style.visibility = 'hidden';
   Alert.style.visibility = 'hidden';
   Reset.style.visibility = 'hidden';
-  console.log(localStorage.getItem("numer"));
-  
 }
 
 function reset()
